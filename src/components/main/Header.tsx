6a5a76e8a108b368ede9a62db1435aae22ade11d
Shimotuki Rierun <rierun@rierun.dev>
2024-07-30 00:35:43 +0900

    work: 🚧 fix mobile view

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <header className={MPlus1p.className + " h-[110px]"}>
-      <div className="h-full flex flex-row justify-center md:justify-between items-center px-5 md:px-12">
+      <div className="h-full flex flex-col md:flex-row justify-center md:justify-between items-center px-5 md:px-12 pb-2 md:pb-0">
         <div className="text-6xl text-[#323232] px-2 py-1 font-bold transition-all duration-500 rounded-sm hover:text-[#4b4d67]">
           <Link href="/">
             rierun
@@ -33,7 +33,7 @@ export default function Header() {
           </Link>
         </div>
         <div>
-          <nav className="flex flex-row gap-4 font-bold text-2xl">
+          <nav className="flex flex-row gap-4 font-normal md:font-bold text-2xl">
             {navItems.map((navItem, index) => (
               <div key={index}>
                 <Link href={navItem.href}>{navItem.name}</Link>
--- src/components/main/mainPage.tsx
+++ src/components/main/mainPage.tsx
@@ -21,12 +21,12 @@ export default function MainPage({
           className={
             MPlus1p.className +
             " " +
-            "h-72 bg-[#5E838E] text-7xl font-bold text-white flex flex-col justify-center pl-28"
+            "h-72 bg-[#5E838E] text-7xl font-bold text-white flex flex-col justify-center pl-12 md:pl-28"
           }
         >
           <h1>{title}</h1>
         </div>
-        <div className={breadcrumbs && "bg-gray-200 py-2 pl-28"}>
+        <div className={breadcrumbs && "bg-gray-200 py-2 pl-12 md:pl-28"}>
           {breadcrumbs && (
             <div className="flex flex-row gap-2 items-center text-xl text-gray-600">
               {breadcrumbs.map(
@@ -46,7 +46,7 @@ export default function MainPage({
             </div>
           )}
         </div>
-        <div className="text-2xl px-28 pt-5">{children}</div>
+        <div className="text-2xl px-12 md:px-28 pt-5">{children}</div>
       </main>
       <Footer />
     </>
