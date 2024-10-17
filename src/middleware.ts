5e7168b7c168ff877f4b0513f890f24da7ffe0ab
Shimotuki Rierun <rierun@rierun.dev>
2024-10-17 19:21:47 +0900

    work: 認証は/apiより始めるように next.jsのアップデートとその他ライブラリの追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 function authMiddleware(request: NextRequest) {
-  const auth = request.headers.get("authorization");
+  if (request.url.startsWith("/api")) {
+    const auth = request.headers.get("authorization");
 
-  if (!auth) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    if (!auth) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const token = auth?.replace("Bearer ", "");
+    if (token !== process.env.SITE_API_KEY) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
   }
-
-  const token = auth?.replace("Bearer ", "");
-  if (token !== process.env.SITE_API_KEY) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
   return NextResponse.next();
 }
 
